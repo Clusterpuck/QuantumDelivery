52dfa6508a250e6b85d13e063c76da0a563b032e
ameliefisher <amelie.fisher@student.curtin.edu.au>
2024-08-07 21:33:22 +0800

    bug fix

--- src/components/AddOrder.jsx
+++ src/components/AddOrder.jsx
@@ -16,7 +16,7 @@ const AddOrder = () => {
         <div style={{ display: "flex", alignItems: "center", gap: 16 }}>
             <Link to="/viewroutes" className="nav-link">View Routes</Link>
             <Link to="/livetracking" className="nav-link">Live Tracking</Link>
-            <Link to="/dailyreport" className="nav-link">Daily Reports</Link>
+            <Link to="/dailyreports" className="nav-link">Daily Reports</Link>
             <Link to="/addorder" className="nav-link">Add Order</Link>
         </div>
         <h1>Add Order</h1>

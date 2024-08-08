e63ef1f0d6bee73caccb1f3850fa24a74b79795d
Nick <nicholas.g.wright86@gmail.com>
2024-08-09 00:28:49 +0800

    Added combo boxes and modal add forms for customer and location

--- src/components/AddOrder.jsx
+++ src/components/AddOrder.jsx
@@ -1,24 +1,57 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { fetchCustomers } from '../store/apiFunctions';
-import { Autocomplete } from '@mui/material';
+import { fetchCustomers, fetchLocations } from '../store/apiFunctions';
+import { Autocomplete, Button, Dialog, DialogActions, DialogContent, Box, Paper, Grid } from '@mui/material';
+import AddressSearch from './AddressSearch';
+import AddCustomer from './AddCustomer';
 import TextField from '@mui/material/TextField';
+
+
+const styleConstants = {
+    fieldSpacing: { mb: 2 }
+};
+
 
 const AddOrder = () => {
     const [customers, setCustomers] = useState(null);
+    const [locations, setLocations] = useState(null);
     const [selectedCustomer, setSelectedCustomer] = useState('');
+    const [selectedLocation, setSelectedLocation] = useState('');
+    const [openAddress, setOpenAddress] = useState(false);
+    const [openCustomer, setOpenCustomer] = useState(false);
 
     useEffect(() => {
-        const loadCustomers = async () => {
+        const loadData = async () => {
             const newCustomers = await fetchCustomers();
             setCustomers(newCustomers);    
+            const newLocations = await fetchLocations();
+            setLocations(newLocations);
         };
-        loadCustomers();
+        loadData();
     }, []);
 
-    const handleChange = (event, newValue) => {
+    const handleCustomerChange = (event, newValue) => {
         setSelectedCustomer(newValue);
         console.log("Selected customer is  " + selectedCustomer.name);
+    };
+
+    const handleLocationChange = (event, newValue) => {
+        setSelectedLocation(newValue);
+        console.log("Selected location is  " + selectedLocation.address);
+    };
+
+
+    const handleClickOpenAddress = () => {
+        setOpenAddress(true);
+    };
+
+    const handleClickOpenCustomer = () => {
+        setOpenCustomer(true);
+    }
+
+    const handleClose = () => {
+        setOpenAddress(false);
+        setOpenCustomer(false);
     };
 
     return (
@@ -39,19 +72,90 @@ const AddOrder = () => {
             </div>
 
             <h1>Add Order</h1>
-            {customers ? (
-                <Autocomplete
-                disablePortal
-                id="Customers"
-                options={customers}
-                getOptionLabel={(option) => option.name}
-                sx={{ width: 400 }}
-                onChange={handleChange}
-                renderInput={(params) => <TextField {...params} label="Select Customer" />}
-                />
-            ) : (
-                <p>No Customers</p>
-            )}
+
+            <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
+                <Paper elevation={3} sx={{ padding: 3, maxWidth: 900, width: '100%' }}>
+                    <Grid container spacing={2} >
+                        <Grid item xs={6} sx={styleConstants.fieldSpacing}>
+                        {customers ? (
+                            <Autocomplete
+                                disablePortal
+                                id="Customers"
+                                options={customers}
+                                getOptionLabel={(option) => option.name}
+                                sx={{ width: 400 }}
+                                onChange={handleCustomerChange}
+                                renderInput={(params) => <TextField {...params} label="Select Customer" />}
+                            />
+                        ) : (
+                            <p>No Customers</p>
+                        )}
+                        </Grid>
+                        <Grid item xs={6} sx={styleConstants.fieldSpacing}>
+
+                        {locations ? (
+                            <Autocomplete
+                                disablePortal
+                                id="Locations"
+                                options={locations}
+                                getOptionLabel={(option) => option.address}
+                                sx={{ width: 400 }}
+                                onChange={handleLocationChange}
+                                renderInput={(params) => <TextField {...params} label="Select Location" />}
+                            />
+                        ) : (
+                            <p>No Customers</p>
+                        )}
+                        </Grid>
+
+                        <Grid item xs={6} sx={styleConstants.fieldSpacing}>
+
+                        {/* Button to open the modal */}
+                        <Button variant="contained" color="primary" onClick={handleClickOpenCustomer}>
+                            Add Customer
+                        </Button>
+
+                        </Grid>
+
+                        <Grid item xs={6} sx={styleConstants.fieldSpacing}>
+
+                        {/* Button to open the modal */}
+                        <Button variant="contained" color="primary" onClick={handleClickOpenAddress}>
+                            Add Address
+                        </Button>
+
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </Box>
+
+            {/* Dialog for AddressSearch */}
+            <Dialog open={openAddress} onClose={handleClose} maxWidth="md" fullWidth>
+                <DialogContent>
+                    <AddressSearch />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Close
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+            <Dialog open={openCustomer} onClose={handleClose} maxWidth="md" fullWidth>
+                <DialogContent>
+                    <AddCustomer />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Close
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+            
+
+
+
             <a href="/">Back Home</a>
         </div>
     );

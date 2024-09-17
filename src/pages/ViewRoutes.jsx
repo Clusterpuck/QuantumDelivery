0700d089b551bd40a98084aa576c5f99fcfcb4b3
Nick <nicholas.g.wright86@gmail.com>
2024-09-17 22:58:59 +0800

    grouped routes by date,, showing all routes in one drop down

--- src/pages/ViewRoutes.jsx
+++ src/pages/ViewRoutes.jsx
@@ -4,10 +4,12 @@ import dayjs from 'dayjs';
 import 'dayjs/locale/en-gb';
 
 // Material-UI Components
-import { TextField, Button, Grid, Paper, MenuItem, Snackbar, 
-  Alert, Divider, Typography, InputAdornment, Radio, RadioGroup,
-  FormControlLabel, Accordion, AccordionDetails, AccordionSummary,
-} from '@mui/material';
+import
+  {
+    TextField, Button, Grid, Paper, MenuItem, Snackbar,
+    Alert, Divider, Typography, InputAdornment, Radio, RadioGroup,
+    FormControlLabel, Accordion, AccordionDetails, AccordionSummary,
+  } from '@mui/material';
 
 // Material-UI Icons
 import RouteIcon from '@mui/icons-material/Route';
@@ -36,7 +38,7 @@ const styleConstants = {
 
 
 // Page design for View Routes page
-const ViewRoutes = ( ) =>
+const ViewRoutes = () =>
 {
   const [selectedDate, setSelectedDate] = useState(dayjs());
   const [unassignedDates, setUnassignedDates] = useState([]);
@@ -54,7 +56,7 @@ const ViewRoutes = ( ) =>
   const [numVehicles, setNumVehicles] = useState(1); // default to 1 vehicle
   const [calcType, setCalcType] = useState("brute");
 
-  
+
 
 
   useEffect(() =>
@@ -74,7 +76,7 @@ const ViewRoutes = ( ) =>
   }, [plannedOrders, selectedDate]);
 
 
-  
+
   /**
    * Function to handle date change and load dummy output
    *
@@ -116,7 +118,7 @@ const ViewRoutes = ( ) =>
   }
 
 
-  
+
   /**
    * Filters all the orders that are planned to only orders
    * in the selected date. 
@@ -143,14 +145,15 @@ const ViewRoutes = ( ) =>
    *
    * @param {*} event
    */
-  const handleCalcChange = (event) => {
+  const handleCalcChange = (event) =>
+  {
     setCalcType(event.target.value)
-    console.log("Calc changed to ", event.target.value );
+    console.log("Calc changed to ", event.target.value);
 
   }
 
 
-  
+
   /** Deals with user requesting closing the snackbar */
   const handleSnackbarClose = () =>
   {
@@ -158,54 +161,39 @@ const ViewRoutes = ( ) =>
   };
 
 
-  
+
   /**
    * If orders are loaded and the datePlanned orders are defined and have values
    * Meaning there are orders that aren't assigned on the selected date
    * A request with those orders are sent to determine the routes
-   * RUns load orders if routes are calculated correctly. 
+   * Runs load orders if routes are calculated correctly. 
    *
    * @async
    * @returns {*}
    */
-  const calcRoutes = async () => {
+  const calcRoutes = async () =>
+  {
     try
     {
       setRoutesLoading(true);
       if (!ordersLoaded) return; // Do not load routes if orders are not loaded
-      if( !datePlannedOrders || datePlannedOrders.length == 0 ) return; 
+      if (!datePlannedOrders || datePlannedOrders.length == 0) return;
       const userInput = {
         numVehicle: numVehicles,
         calcType: calcType,
         deliveryDate: selectedDate,
         orders: datePlannedOrders.
-        map(order => order.orderID) // all orders
+          map(order => order.orderID) // all orders
       };
 
       console.log("Payload being sent: ", JSON.stringify(userInput));
       await postDeliveryRoutes(userInput);
       //needs to return all routes, not just new routes
-      const unsortedRoutesList = await loadRoutes();
-      if (unsortedRoutesList)
-      {
-        const routesList = unsortedRoutesList.sort((a, b) => a.position - b.position);
-        setRoutes(routesList);
-        //send instead of relying on set due to asynch setting
-        
-        //resets order list after them being assigned to routes
-        await loadOrders();
+      await loadRoutes();
+      //send instead of relying on set due to asynch setting
+      //resets order list after them being assigned to routes
+      await loadOrders();
 
-      }
-      else
-      {
-        // throw error
-        console.error('Error fetching delivery routes: ', error);
-        setSnackbar({
-          open: true,
-          message: 'Failed to load delivery routes',
-          severity: 'error'
-        });
-      }
     } catch (error)
     {
       // catch error
@@ -215,7 +203,7 @@ const ViewRoutes = ( ) =>
         message: 'Failed to load delivery routes',
         severity: 'error'
       });
-    }finally
+    } finally
     {
       setRoutesLoading(false);
     }
@@ -235,7 +223,7 @@ const ViewRoutes = ( ) =>
     const ordersList = await fetchMethod("orders");
     if (ordersList)
     {
-      
+
       //console.log("Order list is " + JSON.stringify(ordersList));
       const filteredOrders = ordersList.filter(order => order.status === "PLANNED");
       const sortedOrderList = filteredOrders.sort((a, b) => a.position - b.position);
@@ -259,7 +247,7 @@ const ViewRoutes = ( ) =>
   };
 
 
-  
+
   /**
    * Delete route send a route to be deleted by ID
    * Once sent, routes and orders are reloaded. 
@@ -300,9 +288,25 @@ const ViewRoutes = ( ) =>
       const routesList = await fetchMethod('DeliveryRoutes');
       if (routesList)
       {
-        console.log(JSON.stringify(routesList))
-        setRoutes(routesList);
-        return routesList;
+        console.log("xxXX Route List is " + JSON.stringify(routesList));
+        //setRoutes(routesList);
+        const groupedRoutes = routesList.reduce((acc, route) =>
+        {
+          const deliveryDate = new Date(route.deliveryDate).toDateString(); // Convert to string (ignoring time)
+
+          // Check if this date already exists in the accumulator
+          if (!acc[deliveryDate])
+          {
+            acc[deliveryDate] = []; // Initialize array if it doesn't exist
+          }
+
+          acc[deliveryDate].push(route); // Add route to the relevant date group
+          return acc;
+        }, {});
+
+        setRoutes(groupedRoutes)
+        console.log("xxXXGrouped Routes by Date: ", groupedRoutes);
+        return groupedRoutes;
       }
       else
       {
@@ -327,12 +331,11 @@ const ViewRoutes = ( ) =>
   };
 
 
-
   // Define columns for DataGrid
   const columns = [
     { field: 'orderID', headerName: 'Order ID', width: 90 },
     { field: 'deliveryDate', headerName: 'Delivery Date', width: 100, renderCell: (params) => formatDate(params.value) },
-    { field: 'position', headerName: 'Position', width: 90},
+    { field: 'position', headerName: 'Position', width: 90 },
     { field: 'address', headerName: 'Address', width: 150 },
     { field: 'status', headerName: 'Status', width: 150 },
     { field: 'customerName', headerName: 'Customer Name', width: 150 },
@@ -373,7 +376,7 @@ const ViewRoutes = ( ) =>
           <Grid item xs={12} md={12} container spacing={2} alignItems="center">
             <Grid item xs={6} md={3} >
 
-              <DateSelectHighlight highlightedDates={unassignedDates}  selectedDate={selectedDate} handleDateChange={handleDateChange}/>
+              <DateSelectHighlight highlightedDates={unassignedDates} selectedDate={selectedDate} handleDateChange={handleDateChange} />
 
             </Grid>
             {/* Dropdown for selecting number of vehicles and Regenerate button */}
@@ -398,18 +401,18 @@ const ViewRoutes = ( ) =>
                   </MenuItem>
                 ))}
               </TextField>
-                </Grid>
-              <Grid item xs={6} md={3}>
-                <RadioGroup
-                  aria-labelledby="demo-controlled-radio-buttons-group"
-                  name="controlled-radio-buttons-group"
-                  value={calcType}
-                  onChange={handleCalcChange}
-                >
-                  <FormControlLabel value="brute" control={<Radio />} label="Brute Force" />
-                  <FormControlLabel value="dwave" control={<Radio />} label="Quantum Computer" />
-                </RadioGroup>
-              </Grid>
+            </Grid>
+            <Grid item xs={6} md={3}>
+              <RadioGroup
+                aria-labelledby="demo-controlled-radio-buttons-group"
+                name="controlled-radio-buttons-group"
+                value={calcType}
+                onChange={handleCalcChange}
+              >
+                <FormControlLabel value="brute" control={<Radio />} label="Brute Force" />
+                <FormControlLabel value="dwave" control={<Radio />} label="Quantum Computer" />
+              </RadioGroup>
+            </Grid>
             <Grid item xs={6} md={4} container justifyContent="flex-end">
               {routesLoading ? (
                 <Button
@@ -435,8 +438,8 @@ const ViewRoutes = ( ) =>
             </Grid>
           </Grid>
 
-          <Grid item xs={12} md={12} container spacing={2} alignItems="center">
-            <Accordion style={{ width: '85%' }}>
+          <Grid item xs={12} md={12} container spacing={2} alignItems="center" maxWidth='1200px'>
+            <Accordion style={{ width: '100%' }}>
               <AccordionSummary
                 expandIcon={<ExpandMoreIcon />}
                 aria-controls="panel1-content"
@@ -445,95 +448,115 @@ const ViewRoutes = ( ) =>
                 {plannedOrders ? <p>Unassigned Orders {datePlannedOrders.length}</p> : <p>No Unassigned Orders</p>}
               </AccordionSummary>
               <AccordionDetails>
-                <OrdersTable updateData={false} orders={datePlannedOrders}/>
+                <OrdersTable updateData={false} orders={datePlannedOrders} />
               </AccordionDetails>
             </Accordion>
           </Grid>
 
 
           <Grid item xs={6}>
-          
+
           </Grid>
 
-         
-         {/* Render assigned vehicles */}
-{routes.map((route) => (
-  <Accordion key={route.deliveryRouteID}>
-     <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1-content"
-                id="panel1-header"
-              >
-    <Grid container alignItems="center" spacing={2}>
-      <Grid item xs={10}>
-        <Typography>
-          ID: {route.deliveryRouteID} &nbsp;
-          Vehicle {route.vehicleId} &nbsp;
-          Orders: {route.orders.length}
-        </Typography>
-      </Grid>
-      <Grid item xs={2}>
-        <Button
-          onClick={() => deleteRoute(route.deliveryRouteID)}
-          color="error"
-          variant="contained"
-        >
-          Delete Route
-        </Button>
-      </Grid>
-    </Grid>
-    </AccordionSummary>
-    <AccordionDetails>
-      <Grid item xs={12} sx={styleConstants.fieldSpacing}>
-        <Divider>
-          <Typography variant="h4" component="h3" align="center">
-          
-            Vehicle {route.vehicleId}
-          </Typography>
-        </Divider>
+          <Grid item xs={12} md={12} container spacing={2} alignItems="center" maxWidth='1200px'>
+            {/* Render assigned vehicles */}
+            {Object.entries(routes).map(([date, dateRoutes]) => (
+              <Accordion key={date}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls={`panel-${date}-content`}
+                  id={`panel-${date}-header`}
+                  sx={{
+                    backgroundColor: 'lightblue',  // Set background color
+                    borderRadius: '5px',
+                    borderBottom: '1px solid grey', // Add a border
+                    padding: '1px', // Adjust padding
+                    '&:hover': {
+                      backgroundColor: 'teal', // Hover effect
+                    },
+                    '& .MuiTypography-root': {
+                      fontWeight: 'bold', // Custom font styles for text
+                      color: '#333', // Change text color
+                    },
+                  }}
+                >
+                  <Grid container alignItems="center" spacing={2}>
+                    <Grid item xs={10}>
+                      <Typography variant="h6">
+                        Delivery Date: {date}
+                      </Typography>
+                      <Typography variant="subtitle1">
+                        Total Routes: {dateRoutes.length}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </AccordionSummary>
+                <AccordionDetails>
+                  {dateRoutes.map((route) => (
+                    <>
+                      <Grid container alignItems="center" spacing={2}>
+                        <Grid item xs={10}>
+                          <Typography>
+                            ID: {route.deliveryRouteID} &nbsp;
+                            Vehicle: {route.vehicleId} &nbsp;
+                            Orders: {route.orders.length} &nbsp;
+                            Date: {formatDate(route.deliveryDate)}
+                          </Typography>
+                        </Grid>
+                        <Grid item xs={2}>
+                          <Button
+                            onClick={() => deleteRoute(route.deliveryRouteID)}
+                            color="error"
+                            variant="contained"
+                          >
+                            Delete Route
+                          </Button>
+                        </Grid>
+                      </Grid>
 
-        {/* DataGrid for orders */}
-        <Grid item sx={styleConstants.fieldSpacing}>
-          <DataGrid
-            rows={route.orders
-              .slice() // Copy to avoid mutating original array
-              .sort((a, b) => a.position - b.position) // Sort by position
-              .map((order) => ({ id: order.orderID, ...order }))} // Set the row ID
-            columns={columns}
-            pageSize={5}
-            autoHeight
-          />
+                      <Grid item xs={12} sx={styleConstants.fieldSpacing}>
+                        <Divider>
+                          <Typography variant="h4" component="h3" align="center">
+                            Vehicle {route.vehicleId}
+                          </Typography>
+                        </Divider>
 
-          {/* Map for orders */}
-          {route.orders && route.orders.length > 0 ? (
-            <>
-              <MapWithPins
-                inputLocations={route.orders
-                  .slice() // Copy to avoid mutating the original array
-                  .sort((a, b) => a.position - b.position) // Sort by position
-                  .map((order) => ({
-                    latitude: order.latitude,
-                    longitude: order.longitude
-                  }))}
-              />
-            </>
-          ) : (
-            <p>No Orders</p>
-          )}
+                        {/* DataGrid for orders */}
+                        <Grid item sx={styleConstants.fieldSpacing}>
+                          <DataGrid
+                            rows={route.orders
+                              .slice()
+                              .sort((a, b) => a.position - b.position)
+                              .map((order) => ({ id: order.orderID, ...order }))}
+                            columns={columns}
+                            pageSize={5}
+                            autoHeight
+                          />
 
-          {/* Delete Route Button */}
-          {/* <Button
-            onClick={() => deleteRoute(route.deliveryRouteID)}
-            color="error"
-            variant="contained"
-          >
-            Delete Route
-          </Button> */}
-        </Grid>
-      </Grid>
-    </AccordionDetails>
-  </Accordion>
-))}
+                          {/* Map for orders */}
+                          {route.orders && route.orders.length > 0 ? (
+                            <>
+                              <MapWithPins
+                                inputLocations={route.orders
+                                  .slice()
+                                  .sort((a, b) => a.position - b.position)
+                                  .map((order) => ({
+                                    latitude: order.latitude,
+                                    longitude: order.longitude,
+                                  }))}
+                              />
+                            </>
+                          ) : (
+                            <p>No Orders</p>
+                          )}
+                        </Grid>
+                      </Grid>
+                    </>
+                  ))}
+                </AccordionDetails>
+              </Accordion>
+            ))}
+          </Grid>
 
         </Grid>
       </Paper>

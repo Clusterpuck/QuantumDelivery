0791052c92f8dcf7e5e571efa5e3a497f22e925c
Scrubzie <Scrubzzilla@gmail.com>
2024-10-08 11:08:40 +0800

    Removed unnecessary import

--- src/components/DateSelectHighlight.jsx
+++ src/components/DateSelectHighlight.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { PickersDay, DesktopDatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import TextField from '@mui/material/TextField';
